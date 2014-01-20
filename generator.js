bbe938d47b8b5b7fdd881bc234a7fdb306fd50ed
Joel McCoy <joelmccoy@gmail.com>
2014-01-19 19:26:34 -0500

    Treat + in URL as space

--- generator.js
+++ generator.js
@@ -3,7 +3,7 @@ var express     = require('express'),
     http        = require('http'),
     path        = require('path');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5250);
 app.set('views', path.join(__dirname, 'www', 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon(path.join(__dirname, 'www', 'public', 'favicon.ico')));
@@ -16,13 +16,14 @@ app.get('/', function(req, res) {
   res.render('base', {genWord: "FOOTBALL"});
 });
 
-app.get('/:genWord/', function(req, res) {
-  res.render('base', {genWord: req.params.genWord.toUpperCase()})
-})
+app.get('/:genWord/', toTemplate)
 
-app.get('/:genWord', function(req, res) {
-  res.render('base', {genWord: req.params.genWord.toUpperCase()})
-})
+app.get('/:genWord', toTemplate)
+
+function toTemplate(req, res) {
+  genWord = req.params.genWord.toUpperCase().replace('+',' ')
+  res.render('base', {genWord: genWord})
+}
 
 http.createServer(app)
 .listen(app.get('port'), function(err){
